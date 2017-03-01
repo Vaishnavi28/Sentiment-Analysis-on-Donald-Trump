4982d269b48b82f4cfa7a3de0404b336c23720c8
Vaishnavi Ayyadurai <vaishnavi.ayyadurai@outlook.com>
2017-03-01 13:01:28 -0800

    Add files via upload

---
+++ Sentiment_Analysis.R
@@ -0,0 +1,49 @@
+library(twitteR)
+library(ROAuth)
+require(RCurl)
+library(stringr)
+library(tm)
+library(ggmap)
+library(dplyr)
+library(plyr)
+library(wordcloud)
+library(openssl)
+library(httpuv)
+
+#Getting twitter Authentication:
+key="q9BddECluO1AHwsrk7eJw23rp"
+setwd="C:/Users/vaish/Dropbox/CSU/Data Mining"
+secret="brcrVJhfzyVJ31e1YQlykjCA77IBAnwaWNrhYqAqFrcydpktqe"
+download.file(url="http://curl.haxx.se/ca/cacert.pem",
+              destfile="C:/Users/vaish/Dropbox/CSU/Data Mining/cacert.pem",
+              method="auto")
+authenticate <- OAuthFactory$new(consumerKey=key,
+                                 consumerSecret=secret,
+                                 requestURL="https://api.twitter.com/oauth/request_token",
+                                 accessURL="https://api.twitter.com/oauth/access_token",
+                                 authURL="https://api.twitter.com/oauth/authorize")
+options(httr_oauth_cache=T)
+
+# Set up call back URL to http://127.0.0.1:1410/ in twitter app and
+#set access token and access secret to do local verification instead of browser based verification
+setup_twitter_oauth(key, secret,access_token=NULL,access_secret=NULL)
+save(authenticate, file="twitter authentication.Rdata")
+
+#Get sample tweets from various cities
+
+N=500  # no of tweets to request from each query
+S=200  # radius in miles
+lats=c(38.9,40.7,37.8,39,37.4,28,30,42.4,48,36,32.3,33.5,34.7,33.8,37.2,41.2,46.8,
+       46.6,37.2,43,42.7,40.8,36.2,38.6,35.8,40.3,43.6,40.8,44.9,44.9)
+
+lons=c(-77,-74,-122,-105.5,-122,-82.5,-98,-71,-122,-115,-86.3,-112,-92.3,-84.4,-93.3,
+       -104.8,-100.8,-112, -93.3,-89,-84.5,-111.8,-86.8,-92.2,-78.6,-76.8,-116.2,-98.7,-123,-93)
+
+#cities=DC,New York,San Fransisco,Colorado,Mountainview,Tampa,Austin,Boston,
+#       Seatle,Vegas,Montgomery,Phoenix,Little Rock,Atlanta,Springfield,
+#       Cheyenne,Bisruk,Helena,Springfield,Madison,Lansing,Salt Lake City,Nashville
+#       Jefferson City,Raleigh,Harrisburg,Boise,Lincoln,Salem,St. Paul
+
+donald=do.call(rbind,lapply(1:length(lats), function(i) searchTwitter('Donald+Trump',
+                                                                      lang="en",n=N,resultType="recent",
+                                                                      geocode=paste(lats[i],lons[i],paste0(S,"mi"),sep=","))))
